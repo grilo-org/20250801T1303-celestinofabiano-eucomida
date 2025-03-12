855762d6d15c7bc61947121b7a4e7a93af6c8682
celestinofabiano <fabianoapc@gmail.com>
2025-03-12 12:38:16 -0300

    Adiciona arquivo favicon

--- src/main/java/com/geosapiens/eucomida/controller/advice/GlobalExceptionHandler.java
+++ src/main/java/com/geosapiens/eucomida/controller/advice/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import com.geosapiens.eucomida.exception.OrderNotFoundException;
 import com.geosapiens.eucomida.exception.UserNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -17,29 +18,36 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
+
+    private static final String LOG_MESSAGE = "[{}] Exception occurred: {}";
 
     @ExceptionHandler(AuthenticatedUserNotFoundException.class)
     public ResponseEntity<ErrorResponseDto> handleAuthenticatedUserNotFoundException(
             AuthenticatedUserNotFoundException ex, HttpServletRequest request) {
+        log.error(LOG_MESSAGE, this.getClass().getSimpleName(), ex.getMessage(), ex);
         return buildErrorResponse(ex, HttpStatus.UNAUTHORIZED, request);
     }
 
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<ErrorResponseDto> handleUserNotFoundException(
             UserNotFoundException ex, HttpServletRequest request) {
+        log.error(LOG_MESSAGE, this.getClass().getSimpleName(), ex.getMessage(), ex);
         return buildErrorResponse(ex, HttpStatus.NOT_FOUND, request);
     }
 
     @ExceptionHandler(OrderNotFoundException.class)
-    public ResponseEntity<ErrorResponseDto> handleUserNotFoundException(
+    public ResponseEntity<ErrorResponseDto> handleOrderNotFoundException(
             OrderNotFoundException ex, HttpServletRequest request) {
+        log.error(LOG_MESSAGE, this.getClass().getSimpleName(), ex.getMessage(), ex);
         return buildErrorResponse(ex, HttpStatus.NOT_FOUND, request);
     }
 
     @ExceptionHandler(CourierNotFoundException.class)
     public ResponseEntity<ErrorResponseDto> handleCourierNotFoundException(
             CourierNotFoundException ex, HttpServletRequest request) {
+        log.error(LOG_MESSAGE, this.getClass().getSimpleName(), ex.getMessage(), ex);
         return buildErrorResponse(ex, HttpStatus.NOT_FOUND, request);
     }
 
@@ -54,28 +62,29 @@ public class GlobalExceptionHandler {
                 .findFirst()
                 .orElse(ErrorMessages.VALIDATION_ERROR);
 
+        log.error(LOG_MESSAGE, this.getClass().getSimpleName(), errorMessage, ex);
         return buildErrorResponse(errorMessage, HttpStatus.BAD_REQUEST, request);
     }
 
     @ExceptionHandler(NoResourceFoundException.class)
     public ResponseEntity<ErrorResponseDto> handleResourceNotFoundException(
             NoResourceFoundException ex, HttpServletRequest request) {
-        return buildErrorResponse(ErrorMessages.RESOURCE_NOT_FOUND_ERROR,
-                HttpStatus.BAD_REQUEST, request);
+        log.error(LOG_MESSAGE, this.getClass().getSimpleName(), ex.getMessage(), ex);
+        return buildErrorResponse(ErrorMessages.RESOURCE_NOT_FOUND_ERROR, HttpStatus.BAD_REQUEST, request);
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<ErrorResponseDto> handleMessageNotReadableException(
             HttpMessageNotReadableException ex, HttpServletRequest request) {
-        return buildErrorResponse(ErrorMessages.NOT_READABLE_ERROR,
-                HttpStatus.BAD_REQUEST, request);
+        log.error(LOG_MESSAGE, this.getClass().getSimpleName(), ex.getMessage(), ex);
+        return buildErrorResponse(ErrorMessages.NOT_READABLE_ERROR, HttpStatus.BAD_REQUEST, request);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponseDto> handleGenericException(
             Exception ex, HttpServletRequest request) {
-        return buildErrorResponse(ErrorMessages.INTERNAL_SERVER_ERROR,
-                HttpStatus.INTERNAL_SERVER_ERROR, request);
+        log.error(LOG_MESSAGE, this.getClass().getSimpleName(), ex.getMessage(), ex);
+        return buildErrorResponse(ErrorMessages.INTERNAL_SERVER_ERROR, HttpStatus.INTERNAL_SERVER_ERROR, request);
     }
 
     private ResponseEntity<ErrorResponseDto> buildErrorResponse(
